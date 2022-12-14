54d96c3fdf56b2593e325ef133d792ede6a189f0
Abioye Oluwafemi <oluwafemiabioye@gmail.com>
2022-12-13 19:34:01 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Commputes the absolute value of an integerr
+ * @t: The integer to be computed
+ * Return: The abs 
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
